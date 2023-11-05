d15f426bbea098c46d8b3259000ecd02b18d7bc2
rkm0078 <146083411+rkm0078@users.noreply.github.com>
2023-11-05 21:42:57 +0530

    Update WPSpam.py

--- WPSpam.py
+++ WPSpam.py
@@ -1,8 +1,16 @@
+# Text Spammer bot by RKM
 import pyautogui as pag
 import time as t
 
-t.sleep(4)  # Delay for your clicking
-# Click the where you want to send or write
-for i in range(50):
-    pag.typewrite("Hello")
+x = int(input("Enter time delay: "))
+y = int(input("Enter test limit: "))
+z = input("Enter the text: ")
+
+print(f"Click anywhere in {x} Seconds...")
+
+t.sleep(x)
+
+for i in range(y):
+    pag.typewrite('z')
     pag.press('enter')
+print("Operation Successfull!!")
